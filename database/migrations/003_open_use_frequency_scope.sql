b8d95a98a7a8595eb9f2d0c0c24d0f288f5ca034
Nicolás Montaña <nicolas.montana@alumnos.ucentral.cl>
2026-07-23 14:11:22 -0400

    fix: make open use migration format independent

--- database/migrations/003_open_use_frequency_scope.sql
+++ database/migrations/003_open_use_frequency_scope.sql
@@ -26,37 +26,83 @@ BEGIN
 END
 ELSE
 BEGIN
-    DECLARE @old_from NVARCHAR(MAX) =
-N'    FROM inserted i
-    LEFT JOIN deleted d ON d.id = i.id
-    INNER JOIN dbo.reservations previous WITH (UPDLOCK, HOLDLOCK)';
+    DECLARE @frequency_start INT = CHARINDEX(N'DECLARE @next_request_date DATE;', @definition);
+    DECLARE @frequency_end INT = CHARINDEX(
+        N'IF EXISTS (SELECT 1 FROM inserted i INNER JOIN dbo.users u ON u.id = i.user_id',
+        @definition,
+        @frequency_start
+    );
 
-    DECLARE @new_from NVARCHAR(MAX) =
-N'    FROM inserted i
+    IF @frequency_start = 0 OR @frequency_end = 0 OR @frequency_end <= @frequency_start
+        THROW 53001, 'Preflight: definicion inesperada del trigger; no se modifico.', 1;
+
+    DECLARE @new_frequency NVARCHAR(MAX) =
+N'DECLARE @next_request_date DATE;
+
+    SELECT TOP (1)
+        @next_request_date = DATEADD(
+            DAY,
+            previous_policy.request_frequency_days,
+            CONVERT(
+                DATE,
+                previous.created_at AT TIME ZONE ''UTC'' AT TIME ZONE ''Pacific SA Standard Time''
+            )
+        )
+    FROM inserted i
     LEFT JOIN deleted d ON d.id = i.id
     INNER JOIN dbo.resources inserted_resource ON inserted_resource.id = i.resource_id
-    INNER JOIN dbo.reservations previous WITH (UPDLOCK, HOLDLOCK)';
-
-    DECLARE @old_policy_join NVARCHAR(MAX) =
-N'       AND previous.status IN (''PENDING'', ''CONFIRMED'')
-    INNER JOIN dbo.reservation_policies previous_policy ON previous_policy.id = previous.policy_id
-    WHERE d.id IS NULL
-      AND CONVERT(DATE, i.created_at';
-
-    DECLARE @new_policy_join NVARCHAR(MAX) =
-N'       AND previous.status IN (''PENDING'', ''CONFIRMED'')
+    INNER JOIN dbo.reservations previous WITH (UPDLOCK, HOLDLOCK)
+        ON previous.user_id = i.user_id
+       AND previous.id <> i.id
+       AND previous.status IN (''PENDING'', ''CONFIRMED'')
     INNER JOIN dbo.resources previous_resource ON previous_resource.id = previous.resource_id
     INNER JOIN dbo.reservation_policies previous_policy ON previous_policy.id = previous.policy_id
     WHERE d.id IS NULL
       AND inserted_resource.reservation_mode <> ''OPEN_USE''
       AND previous_resource.reservation_mode <> ''OPEN_USE''
-      AND CONVERT(DATE, i.created_at';
+      AND CONVERT(DATE, i.created_at AT TIME ZONE ''UTC'' AT TIME ZONE ''Pacific SA Standard Time'') < DATEADD(
+          DAY,
+          previous_policy.request_frequency_days,
+          CONVERT(
+              DATE,
+              previous.created_at AT TIME ZONE ''UTC'' AT TIME ZONE ''Pacific SA Standard Time''
+          )
+      )
+    ORDER BY DATEADD(
+        DAY,
+        previous_policy.request_frequency_days,
+        CONVERT(
+            DATE,
+            previous.created_at AT TIME ZONE ''UTC'' AT TIME ZONE ''Pacific SA Standard Time''
+        )
+    ) DESC, previous.id DESC;
 
-    IF CHARINDEX(@old_from, @definition) = 0 OR CHARINDEX(@old_policy_join, @definition) = 0
-        THROW 53001, 'Preflight: definicion inesperada del trigger; no se modifico.', 1;
+    IF @next_request_date IS NOT NULL
+    BEGIN
+        DECLARE @frequency_message NVARCHAR(2048) = CONCAT(
+            N''Ya existe una solicitud vigente. Proxima fecha permitida: '',
+            CONVERT(NVARCHAR(10), @next_request_date, 23),
+            N''.''
+        );
+        THROW 51010, @frequency_message, 1;
+    END;
 
-    SET @definition = REPLACE(@definition, @old_from, @new_from);
-    SET @definition = REPLACE(@definition, @old_policy_join, @new_policy_join);
+    ';
+
+    SET @definition =
+        LEFT(@definition, @frequency_start - 1)
+        + @new_frequency
+        + SUBSTRING(@definition, @frequency_end, LEN(@definition) - @frequency_end + 1);
+
+    DECLARE @create_or_alter INT = CHARINDEX(N'CREATE OR ALTER TRIGGER', UPPER(@definition));
+    DECLARE @create_only INT = CHARINDEX(N'CREATE TRIGGER', UPPER(@definition));
+    IF @create_or_alter > 0
+        SET @definition = STUFF(@definition, @create_or_alter, LEN(N'CREATE OR ALTER TRIGGER'), N'ALTER TRIGGER');
+    ELSE IF @create_only > 0
+        SET @definition = STUFF(@definition, @create_only, LEN(N'CREATE TRIGGER'), N'ALTER TRIGGER');
+    ELSE
+        THROW 53002, 'Preflight: no se pudo identificar la sentencia CREATE del trigger.', 1;
+
     EXEC sys.sp_executesql @definition;
 END;
 GO
